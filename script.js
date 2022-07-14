fb4506919bb920dfd83cb1eaa6952c744755e15e
slavskrit <dpronin@yandex.ru>
2022-07-13 20:09:16 -0700

    Working rotation and mesh and +- shading

--- script.js
+++ script.js
@@ -2,23 +2,8 @@
 
 /* global THREE */
 
-const path = "https://threejs.org/examples/textures/cube/SwedishRoyalCastle/";
-const format = ".jpg";
-const urls = [
-  path + "px" + format,
-  path + "nx" + format,
-  path + "py" + format,
-  path + "ny" + format,
-  path + "pz" + format,
-  path + "nz" + format,
-];
-
-const reflectionCube = new THREE.CubeTextureLoader().load(urls);
-const refractionCube = new THREE.CubeTextureLoader().load(urls);
-refractionCube.mapping = THREE.CubeRefractionMapping;
-
+const SPEED = 0.01;
 const scene = new THREE.Scene();
-// scene.background = reflectionCube;
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -27,43 +12,61 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-const ambient = new THREE.AmbientLight(0x344322);
-const pointLight = new THREE.PointLight(0xfff333, 15);
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshLambertMaterial({
-  color: 0xff6600,
-  combine: THREE.MixOperation,
-  envMap: reflectionCube,
-  reflectivity: 0.1,
-});
+const mControls = new THREE.OrbitControls(camera, renderer.domElement);
+const pointLight = new THREE.PointLight(0xffbf00, 2);
+pointLight.position.z = 2500;
+scene.add(pointLight);
 
-const cube = new THREE.Mesh(geometry, material);
+const pointLight2 = new THREE.PointLight(0xffcf40, 1);
+pointLight2.position.x = 400;
+pointLight2.position.y = 400;
+camera.add(pointLight2);
+
+const pointLight3 = new THREE.PointLight(0xffdc73, 4);
+pointLight3.position.x = -1000;
+pointLight3.position.z = 1000;
+scene.add(pointLight3);
+
 const loader = new THREE.GLTFLoader();
-let parts = [];
-loader.load( 'planet.glb', function ( gltf ) {
-	scene.add( gltf.scene );
-  parts = gltf.scene.children;
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} ); 
+const parts = {};
+let partsNumber = 0;
+loader.load(
+  "planet.glb",
+  function (gltf) {
+    scene.add(gltf.scene);
+    gltf.scene.children.forEach(function (mesh, index) {
+      if (mesh.isMesh) {
+        mesh.material.color = 0x000000;
+        mesh.material.roughness = 0.2;
+        parts[index] = {
+          "mesh": mesh,
+          "vector": new THREE.Vector3(
+            Math.random() - 0.5,
+            Math.random() - 0.5,
+            Math.random() - 0.5,
+          ),
+        };
+      }
+    });
+    partsNumber = gltf.scene.children.length;
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  },
+);
 
 scene.add(pointLight);
-scene.add(ambient);
-
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.z = 3;
 
 function animate() {
-  cube.rotation.y += 0.01;
-  if (parts.length > 0) {
-    for(let i = 0; i < parts.length; i++) {
-      parts[i].rotation.x += 0.01;
-      parts[i].rotation.y += 0.01;
-      parts[i].rotation.z += 0.01;
+  if (partsNumber > 0) {
+    for (let i = 0; i < partsNumber; i++) {
+      parts[i].mesh.rotateOnAxis(parts[i].vector, SPEED);
     }
   }
+  mControls.update();
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
