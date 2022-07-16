5a41aaca9cfbfa7311b29758d6b1b40e642dd03a
slavskrit <dpronin@yandex.ru>
2022-07-16 15:36:52 -0700

    Changed drawing behavior

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const c = document.getElementById("canvas");
-const w = 500, h = 500, pointsCount = 1000;
+const w = 500, h = 500, pointsCount = 5000;
 c.width = w;
 c.height = h;
 const ctx = c.getContext("2d");
@@ -15,20 +15,22 @@ function getRandom(minimum, maximum) {
   return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 }
 
-function draw(p, i) {
+function draw(c, n) {
   ctx.beginPath();
-  ctx.moveTo(w / 2, h / 2);
-  ctx.lineTo(p[i][0], p[i][1]);
-  ctx.globalAlpha = getRandom(0, i % pointsCount) / 1000;
+  ctx.lineTo(c[0], c[1]);
+  ctx.lineTo(n[0], n[1]);
+  ctx.globalAlpha = getRandom(0, 100) / 100;
   ctx.stroke();
 }
 
 let index = 0;
-
+let current = [0, 0];
 function render() {
   window.requestAnimationFrame(render);
-  draw(points, index);
-  index += 1;
+  const next = points[index + 1];
+  draw(current, next);
+  current = next;
+  index += 2;
   if (index == pointsCount) {
     index = 0;
     ctx.clearRect(0, 0, w, h);
